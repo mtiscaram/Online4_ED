4f10bf59d73cc435d777ae0afabea5e9168b2a4c
mtiscaram <123408443+mtiscaram@users.noreply.github.com>
2023-04-09 15:23:44 +0200

    proyecto sin documentar

---
+++ NotaMediaFinal.java
@@ -0,0 +1,26 @@
+package notaMedia;
+
+/**
+ * 
+ * @author mtisc
+ * 
+ */
+public class NotaMediaFinal extends Notas {
+    
+       
+    
+    public static void main(String[] args) {
+
+        
+        double nota1 = primeraNota();
+        double nota2 = segundaNota();
+        double nota3 = terceraNota();
+        calculoNotaMedia(nota1, nota2, nota3);
+      
+    }
+
+    
+
+    
+
+}
