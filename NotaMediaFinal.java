f62e1a482d0ab68fa711c18621f86a293f90712d
mtiscaram <123408443+mtiscaram@users.noreply.github.com>
2023-04-09 16:03:11 +0200

    proyecto documentado

--- NotaMediaFinal.java
+++ NotaMediaFinal.java
@@ -1,17 +1,39 @@
 package notaMedia;
 
 /**
- * 
+ * Clase principal extendida de la superclase Notas
+ *
  * @author mtisc
- * 
+ * @version 09.03.2023
  */
 public class NotaMediaFinal extends Notas {
     
-       
     
+
+    
+    /**
+     * Variables de tipo double
+     * <ol>
+     * <li>nota1</li>
+     * <li>nota2</li>
+     * <li>nota3</li>
+     * </ol>
+     * que se obtienen llamando a diferentes métodos
+     */
+    /**
+     * Diferentes métodos que se llaman de la clase Notas
+     *
+     * @see primeraNota()
+     * @see segundaNota()
+     * @see terceraNota()
+     * @see CalculoNotaMedia(nota1, nota2, nota3)
+     */
+    /**
+     *
+     * @param args
+     */
     public static void main(String[] args) {
 
-        
         double nota1 = primeraNota();
         double nota2 = segundaNota();
         double nota3 = terceraNota();
